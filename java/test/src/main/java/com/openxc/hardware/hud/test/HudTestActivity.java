e1afe300d976f92817311f93185eb34b6d82c2a4
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-03-07 12:15:58 -0500

    Implement HUD test activity using hypothetical HudService.

--- java/test/src/main/java/com/openxc/hardware/hud/test/HudTestActivity.java
+++ java/test/src/main/java/com/openxc/hardware/hud/test/HudTestActivity.java
@@ -1,74 +1,96 @@
 package com.openxc.hardware.hud.test;
 
 import java.util.Map;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+
+import android.os.IBinder;
 import android.util.Log;
 import android.app.Activity;
-import com.openxc.hardware.hud.BTLedBar;
 
 public class HudTestActivity extends Activity {
     private final static String TAG = "HudTest";
+	private final long PERIOD = 500;
+    private final String HUD_MAC_ADDRESS = "000666430D08";
+    private HudService mService;
+	private Blinker mBlinker;
+    private boolean mIsBound;
 
-	final long PERIOD = 500;
-	private BTLedBar hud;
-	private blinker blink;
+    private class Blinker implements Runnable {
+        private boolean mRunning = true;
 
-	public class blinker implements Runnable {
-		private Thread t;
-
-		public blinker(){
-			t = new Thread(this, "blinker");
-			t.start();
-		}
-
-		public void stop(){
-			t.interrupt();
+		public void stop() {
+            mRunning = false;
+            mService.setAll(0.0);
+            mService.disconnect();
 		}
 
 		@Override
 		public void run() {
-			while(true){
+			while(mRunning){
 				for (int i=0;i<5;i++){
 					if (i == 0) {
-						hud.fade(4, PERIOD, 0.0);
+						mService.fade(4, PERIOD, 0.0);
                     } else {
-						hud.fade(i-1, PERIOD, 0.0);
+						mService.fade(i-1, PERIOD, 0.0);
                     }
 
-					hud.fade(i, PERIOD, 1.0);
+					mService.fade(i, PERIOD, 1.0);
 					try {
 						Thread.sleep(PERIOD+Math.round(PERIOD/10));
 					} catch (InterruptedException e) {return;}
 				}
-				Log.d(TAG, "Raw battery level: "+hud.rawBatteryLevel());
+				Log.d(TAG, "Raw battery level: "+mService.rawBatteryLevel());
 			}
 		}
-	}
+	};
+
+    private ServiceConnection mConnection = new ServiceConnection () {
+        public void onServiceConnected(ComponentName classNAme, IBinder service) {
+            mService = ((HudService.LocalBinder)service).getService();
+            mService.setMac(HUD_MAC_ADDRESS);
+            mBlinker.start();
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            mService = null;
+            mBlinker.stop();
+        }
+    };
+
+    private void bindService() {
+        bindService(new Intent(this, HudService.class),
+                mConnection, Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+    }
+
+    private void unbindService() {
+        if(mIsBound) {
+            unbindService(mConnection);
+            mIsBound = false;
+        }
+    }
 
     @Override
     public void onResume() {
-        hud = new BTLedBar();
-        Log.i(TAG, "Connecting to \"RN42\"");
-
-        //repeatedly call setName until a device containing the text
-        //"FireFly" is found.
-        while(!hud.setMac("000666430D08")){
-            Log.i(TAG, "Couldn't find device, retrying");
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) { return; }
-        }
-
-        Log.i(TAG, "Connected, toggling LED");
-        blink = new blinker();
+        mBlinker = new Blinker();
     }
 
     @Override
     public void onPause() {
-        blink.stop();
-        Log.d(TAG, "nuked thread, setting all off");
-        hud.setAll(0.0);
-        Log.i(TAG, "Disconnecting...");
-        //Disconnect will cease automatically reconnecting to device
-        hud.disconnect();
+        mBlinker.stop();
+    }
+
+    @Override
+    public void onStart() {
+        bindService();
+    }
+
+    @Override
+    public void onDestroy() {
+        unbindService();
     }
 }
